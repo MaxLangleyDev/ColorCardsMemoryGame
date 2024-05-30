3e49a84f7d9f3916c5a2e4574d6e58ec7863f1f0
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-05-29 19:17:53 -0700

    Update App.kt

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,12 +1,14 @@
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -15,12 +17,63 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
 
+        val viewModel = viewModel { GameViewModel(
+            initialGameState = GameState(
+                cards = listOf(
+                    GameCard(
+                        cardNumber = 1,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 2,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 3,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 4,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 5,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 6,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 7,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 8,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 9,
+                        isFlipped = false
+                    ),
+                    GameCard(
+                        cardNumber = 10,
+                        isFlipped = false
+                    ),
+                )
+            )
+        ) }
+        val gameState = viewModel.gameState.collectAsState()
+
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
             
-            CardGrid()
+            CardGrid(
+                cards = gameState.value.cards,
+                onCardFlipped = viewModel::flipCard
+            )
 
         }
 
@@ -30,18 +83,17 @@ fun App() {
 @Composable
 fun CardGrid(
     modifier: Modifier = Modifier,
-    cards: List<CardState> = listOf()
+    cards: List<GameCard> = listOf(),
+    onCardFlipped: (index: Int) -> Unit = {}
 ){
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-    ){
-        items(10){
-            val cardState = remember { mutableStateOf(CardState()) }
+    ) {
+        itemsIndexed(cards) { index, card ->
             CardFlippable(
-                state = cardState.value,
+                state = card,
                 onFlipped = {
-                    cardState.value =
-                        cardState.value.copy(isFlipped = !cardState.value.isFlipped)
+                    onCardFlipped(index)
                 }
 
             )
