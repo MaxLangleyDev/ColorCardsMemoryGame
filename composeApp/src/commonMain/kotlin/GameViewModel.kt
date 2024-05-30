1ec94519b80dcd92687c303f50df2ab17f95f151
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-05-30 13:31:25 -0700

    Update GameViewModel.kt

--- composeApp/src/commonMain/kotlin/GameViewModel.kt
+++ composeApp/src/commonMain/kotlin/GameViewModel.kt
@@ -1,7 +1,9 @@
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class GameViewModel(
     initialGameState: GameState = GameState()
@@ -17,6 +19,57 @@ class GameViewModel(
                 isFlipped = !mutableCardsList[cardIndex].isFlipped
             )
         _gameState.value = _gameState.value.copy(cards = mutableCardsList)
+    }
+    fun checkForMatch() {
+
+    }
+
+    fun flipAllCardsDown() {
+        val mutableCardsList = _gameState.value.cards.toMutableList()
+
+        mutableCardsList.forEachIndexed { index, gameCard ->
+            mutableCardsList[index] = gameCard.copy(isFlipped = true)
+        }
+
+    }
+
+    fun flipAllCardsUp() {
+        val mutableCardsList = _gameState.value.cards.toMutableList()
+
+        mutableCardsList.forEachIndexed { index, gameCard ->
+            mutableCardsList[index] = gameCard.copy(isFlipped = false)
+        }
+
+    }
+
+    fun startGame(
+        amountOfCards: Int,
+    ) {
+        val cards = mutableListOf<GameCard>()
+        val colors = listOf(Color.Red, Color.Blue, Color.Green, Color.Yellow)
+
+        var currentColorIndex = 0
+
+        repeat(amountOfCards) { index ->
+            cards.add(
+                GameCard(
+                    cardNumber = index + 1,
+                    isFlipped = true,
+                    color = colors[currentColorIndex]
+                )
+            )
+
+            currentColorIndex = (currentColorIndex + 1) % colors.size
+        }
+
+        cards.shuffle()
+
+        _gameState.update { gameState ->
+            gameState.copy(
+                cards = cards,
+                gameStarted = true
+            )
+        }
     }
 
 
