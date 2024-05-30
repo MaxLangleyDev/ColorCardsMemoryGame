b11097b1c5f79d11e345fb063e888b1a932e8fc6
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-05-30 13:31:23 -0700

    Update GameState.kt

--- composeApp/src/commonMain/kotlin/GameState.kt
+++ composeApp/src/commonMain/kotlin/GameState.kt
@@ -1,6 +1,9 @@
 data class GameState(
 
     val cards: List<GameCard> = listOf(),
-    val points: Int = 0
+    val cardsToFind: List<Int> = listOf(), // List of indices in the cards list,
+    val points: Int = 0,
+
+    val gameStarted: Boolean = false,
 
 )
