7b5e03f65daaa6824f8555e0e5797a5be984048b
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-03 16:01:21 -0700

    Delete GameCard.kt

--- composeApp/src/commonMain/kotlin/GameCard.kt
+++
@@ -1,9 +0,0 @@
-import androidx.compose.ui.graphics.Color
-
-data class GameCard(
-    val cardNumber: Int = -1,
-    val cardName: String = "",
-    val color: Color = Color.Red,
-    val isFlipped: Boolean = false,
-    val isCorrect: Boolean = false
-)
