bf73feaea3632642b78ac297063e014583eefd58
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-03 16:01:23 -0700

    Update GameState.kt

--- composeApp/src/commonMain/kotlin/GameState.kt
+++ composeApp/src/commonMain/kotlin/GameState.kt
@@ -2,7 +2,7 @@ import androidx.compose.ui.graphics.Color
 
 data class GameState(
 
-    val cards: List<GameCard> = listOf(),
+    val cards: List<CardState> = listOf(),
     val colorToFind: Color = Color(0x000000),
     val points: Int = 0,
 
