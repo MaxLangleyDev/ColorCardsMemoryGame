3071105004cd9558f5f1bd00452b1c7c13a68666
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-04 15:27:16 -0700

    Update GameDetailsBar.kt

--- composeApp/src/commonMain/kotlin/presentation/components/GameDetailsBar.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/GameDetailsBar.kt
@@ -1,14 +1,21 @@
 package presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import model.GameState
 
 @Composable
@@ -22,22 +29,48 @@ fun GameDetailsBar(
         verticalArrangement = Arrangement.Center
     ) {
 
-        if (gameState.phasePregame){
+        if (gameState.pregamePhase){
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(4.dp),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ){
                 Text(text = "Memorize Each Color's Location!")
             }
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(4.dp),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ){
                 Text(text = "Time Remaining:")
-                LinearProgressIndicator(progress = gameState.pregameCountdown)
+                Spacer(modifier = Modifier.width(8.dp))
+                LinearProgressIndicator(
+                    progress = { gameState.pregameCountdown },
+                )
             }
+        }
+        else if (gameState.gamePhase){
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(4.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(text = "Find: ")
+                Spacer(modifier = Modifier.width(4.dp))
+                Box(Modifier.size(20.dp).background(color = gameState.colorToFind))
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(4.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(text = "Time Remaining:")
+                Spacer(modifier = Modifier.width(8.dp))
+                LinearProgressIndicator(
+                    progress = { gameState.gameCountdown },
+                )
+            }
+
         }
 
     }
