23c0717a4a6610e6a0de92bf7ab6f19b1aca3af3
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-04 10:30:59 -0700

    Create StartScreen.kt

---
+++ composeApp/src/commonMain/kotlin/presentation/components/StartScreen.kt
@@ -0,0 +1,41 @@
+package presentation.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import model.GameState
+
+@Composable
+fun StartScreen(
+    modifier: Modifier = Modifier.fillMaxSize(),
+    gameState: GameState,
+    setupGame: (amountOfCards: Int) -> Unit = {},
+    startGame: () -> Unit = {}
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Button(
+            onClick = {
+                setupGame(12)
+                startGame()
+            },
+        ){
+            Text(text = "Start Game")
+        }
+
+    }
+}
