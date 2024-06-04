9ccdf4f77d968632efbc26ca5efab4f074f01453
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-03 20:49:55 -0700

    Create CardState.kt

---
+++ composeApp/src/commonMain/kotlin/model/CardState.kt
@@ -0,0 +1,13 @@
+package model
+
+import androidx.compose.ui.graphics.Color
+
+data class CardState(
+    val cardNumber: Int = -1,
+    val cardName: String = "",
+    val color: Color = Color.Red,
+    val isFlipped: Boolean = false,
+    val isCorrect: Boolean = false,
+    val isSelectable: Boolean = true,
+    val isSelected: Boolean = false
+)
