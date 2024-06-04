9d78dfbae9c26c02d1fd1ef3e556768bec5f36b6
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-04 15:27:12 -0700

    Update App.kt

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,14 +1,19 @@
-import androidx.compose.material.MaterialTheme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import presentation.GameContainer
+import ui.theme.ColorCardsTheme
 
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        GameContainer()
+    ColorCardsTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            GameContainer()
+        }
     }
 }
 
