41af99c8da9b1434f6d93f01f5739d3f86cbdc51
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-04 11:15:16 -0700

    Update GameDetailsBar.kt

--- composeApp/src/commonMain/kotlin/presentation/components/GameDetailsBar.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/GameDetailsBar.kt
@@ -1,11 +1,45 @@
 package presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import model.GameState
 
 @Composable
 fun GameDetailsBar(
     modifier: Modifier = Modifier,
+    gameState: GameState = GameState()
 ){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        if (gameState.phasePregame){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(text = "Memorize Each Color's Location!")
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(text = "Time Remaining:")
+                LinearProgressIndicator(progress = gameState.pregameCountdown)
+            }
+        }
+
+    }
 
 }
