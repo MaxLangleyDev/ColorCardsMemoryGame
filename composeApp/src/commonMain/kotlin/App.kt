3f399f83d8fa8a1e03d5910fa76816297141a56d
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-04 11:15:10 -0700

    Update App.kt

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,34 +1,14 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import model.GameState
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import presentation.GameScreen
-import presentation.components.GameBoard
-import presentation.GameViewModel
-import presentation.components.StartScreen
+import presentation.GameContainer
 
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        GameScreen()
+        GameContainer()
     }
 }
 
