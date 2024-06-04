8b3ab4e6196c26bbf97d5168be44c4d47c9272ef
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-03 20:50:01 -0700

    Update GameCard.kt

--- composeApp/src/commonMain/kotlin/presentation/GameCard.kt
+++ composeApp/src/commonMain/kotlin/presentation/GameCard.kt
@@ -1,8 +1,14 @@
 package presentation
 
-import CardState
+import model.CardState
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -18,7 +24,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -36,12 +44,26 @@ fun GameCard(
     card: CardState = CardState(),
     onFlipped: () -> Unit
 ) {
+
     val density = LocalDensity.current.density
+    var shakeCard by remember { mutableStateOf(false) }
+    val cameraDistance by remember { mutableStateOf(8 * density) }
 
     val rotationY by animateFloatAsState(
         targetValue = if (card.isFlipped) 180f else 0f,
         animationSpec = tween(durationMillis = 600)
     )
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val rotationCoefficient = infiniteTransition.animateFloat(
+        initialValue = -1f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 150, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val shadowElevation by animateFloatAsState(100f)
 
     Box(
         modifier = modifier
@@ -53,21 +75,32 @@ fun GameCard(
         contentAlignment = Alignment.Center
     ) {
         if (rotationY <= 90f) {
-            FrontSide(modifier = modifier, card = card, rotationY = rotationY, density = density)
+            shakeCard = card.isSelected && card.isCorrect
+            FrontSide(
+                modifier = modifier,
+                card = card,
+                rotationY = rotationY,
+                rotationZ = if (shakeCard && rotationY != 0f) rotationCoefficient.value * 30f else 0f,
+                cameraDistance = cameraDistance,
+                shadowElevation = shadowElevation
+            )
+
         } else {
             BackSide(modifier = modifier, rotationY = rotationY, density =  density)
         }
     }
 }
 
+
 @Composable
 fun FrontSide(
     modifier: Modifier = Modifier,
     card: CardState,
     rotationY: Float,
-    density: Float
+    rotationZ: Float,
+    cameraDistance: Float,
+    shadowElevation: Float
 ) {
-
     val animatedBorderColor by animateColorAsState(
         targetValue = if (card.isCorrect && card.isSelected) Color.Magenta else card.color,
         animationSpec = tween(durationMillis = 600)
@@ -78,20 +111,21 @@ fun FrontSide(
             .fillMaxSize()
             .graphicsLayer(
                 rotationY = rotationY,
-                cameraDistance = 8 * density
+                rotationZ = rotationZ,
+                cameraDistance = cameraDistance,
+                shadowElevation = shadowElevation
             )
             .clip(RoundedCornerShape(8.dp))
             .background(card.color)
             .border(
                 width = 4.dp,
                 color = animatedBorderColor,
-//                if (card.isCorrect && card.isSelected) Color.Magenta
-//                else card.color,
-                shape = RoundedCornerShape(8.dp))
+                shape = RoundedCornerShape(8.dp)
+            )
             .alpha(if (rotationY <= 90f) 1f else 0f),
         contentAlignment = Alignment.Center
     ) {
-        if (card.isCorrect){
+        if (card.isCorrect) {
             Icon(
                 imageVector = Icons.Default.Star,
                 contentDescription = "Star Icon",
@@ -100,6 +134,46 @@ fun FrontSide(
         }
     }
 }
+//@Composable
+//fun FrontSide(
+//    modifier: Modifier = Modifier,
+//    card: CardState,
+//    rotationY: Float,
+//    density: Float
+//) {
+//
+//    val animatedBorderColor by animateColorAsState(
+//        targetValue = if (card.isCorrect && card.isSelected) Color.Magenta else card.color,
+//        animationSpec = tween(durationMillis = 600)
+//    )
+//
+//    Box(
+//        modifier = modifier
+//            .fillMaxSize()
+//            .graphicsLayer(
+//                rotationY = rotationY,
+//                cameraDistance = 8 * density
+//            )
+//            .clip(RoundedCornerShape(8.dp))
+//            .background(card.color)
+//            .border(
+//                width = 4.dp,
+//                color = animatedBorderColor,
+////                if (card.isCorrect && card.isSelected) Color.Magenta
+////                else card.color,
+//                shape = RoundedCornerShape(8.dp))
+//            .alpha(if (rotationY <= 90f) 1f else 0f),
+//        contentAlignment = Alignment.Center
+//    ) {
+//        if (card.isCorrect){
+//            Icon(
+//                imageVector = Icons.Default.Star,
+//                contentDescription = "Star Icon",
+//                tint = Color.White
+//            )
+//        }
+//    }
+//}
 
 @Composable
 fun BackSide(
