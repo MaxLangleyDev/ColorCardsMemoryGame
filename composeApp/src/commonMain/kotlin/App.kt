69157935c5c15d8358a362db320970a3a23b0f2e
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-04 10:30:48 -0700

    Update App.kt

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -18,72 +18,19 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import model.GameState
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import presentation.GameBoard
+import presentation.GameScreen
+import presentation.components.GameBoard
 import presentation.GameViewModel
+import presentation.components.StartScreen
 
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-
-        val viewModel = viewModel { GameViewModel() }
-
-        val gameState by viewModel.gameState.collectAsState()
-
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-
-            StartScreen(
-                gameState = gameState,
-                setupGame = viewModel::setupGame,
-                startGame = viewModel::startGame
-            )
-
-            GameBoard(
-                modifier = Modifier.fillMaxSize(),
-                gameState = gameState,
-                onCardFlipped = viewModel::userFlipCard
-            )
-
-        }
-
+        GameScreen()
     }
 }
 
-@Composable
-fun StartScreen(
-    modifier: Modifier = Modifier.fillMaxSize(),
-    gameState: GameState,
-    setupGame: (amountOfCards: Int) -> Unit = {},
-    startGame: () -> Unit = {}
-) {
-    AnimatedVisibility(
-        visible = gameState.showStartScreen,
-        enter = slideInVertically(),
-        exit = slideOutVertically(
-            animationSpec = tween(durationMillis = 300, easing = LinearOutSlowInEasing)
-        )
-    ){
-        Column(
-            modifier = modifier,
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-            Button(
-                onClick = {
-                    setupGame(12)
-                    startGame()
-                },
-            ){
-                Text(text = "Start Game")
-            }
-
-        }
-    }
-}
 
 
