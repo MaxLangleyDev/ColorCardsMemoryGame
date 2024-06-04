36719b8ccc478fee147c683f64db8143010a6572
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-04 10:30:58 -0700

    Create GameDetailsBar.kt

---
+++ composeApp/src/commonMain/kotlin/presentation/components/GameDetailsBar.kt
@@ -0,0 +1,11 @@
+package presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GameDetailsBar(
+    modifier: Modifier = Modifier,
+){
+
+}
