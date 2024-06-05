8e1908b4797b7d398279fd88862f9579ab8f01b9
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-05 11:17:36 -0700

    Update GameScreen.kt

--- composeApp/src/commonMain/kotlin/presentation/components/GameScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/GameScreen.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -22,15 +24,24 @@ import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import colorcardsmemorygame.composeapp.generated.resources.Res
 import colorcardsmemorygame.composeapp.generated.resources.game_over
+import colorcardsmemorygame.composeapp.generated.resources.good_luck
+import colorcardsmemorygame.composeapp.generated.resources.im_ready
 import colorcardsmemorygame.composeapp.generated.resources.perfect_game
 import colorcardsmemorygame.composeapp.generated.resources.points
 import colorcardsmemorygame.composeapp.generated.resources.restart
 import colorcardsmemorygame.composeapp.generated.resources.return_to_menu
 import colorcardsmemorygame.composeapp.generated.resources.three_strikes
 import colorcardsmemorygame.composeapp.generated.resources.timed_out
+import colorcardsmemorygame.composeapp.generated.resources.tutorial_message1
+import colorcardsmemorygame.composeapp.generated.resources.tutorial_message2
+import colorcardsmemorygame.composeapp.generated.resources.tutorial_message3
+import colorcardsmemorygame.composeapp.generated.resources.tutorial_title
 import colorcardsmemorygame.composeapp.generated.resources.you_got_x_out_of_y
 import colorcardsmemorygame.composeapp.generated.resources.you_won
 import model.GameState
@@ -46,7 +57,8 @@ fun GameScreen(
     gameState: GameState,
     onCardFlipped: (Int) -> Unit,
     onRestart: () -> Unit,
-    onReturnToMenu: () -> Unit
+    onReturnToMenu: () -> Unit,
+    dismissTutorial: () -> Unit
 ){
     Column(
         modifier = modifier,
@@ -102,6 +114,20 @@ fun GameScreen(
             gameState = gameState,
             onRestart = { onRestart() },
             onReturnToMenu = { onReturnToMenu() }
+        )
+    }
+
+    AnimatedVisibility(
+        visible = gameState.showTutorialMessage,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ){
+        TutorialScreen(
+            modifier = modifier,
+            gameState = gameState,
+            onRestart = { onRestart() },
+            onReturnToMenu = { onReturnToMenu() },
+            dismissTutorial = { dismissTutorial() }
         )
     }
 }
@@ -194,6 +220,55 @@ fun GameWonScreen(
 
             Button(onClick = onRestart) {
                 Text(text = stringResource(Res.string.restart))
+            }
+
+            Button(onClick = onReturnToMenu) {
+                Text(text = stringResource(Res.string.return_to_menu))
+            }
+        }
+    }
+}
+
+@Composable
+fun TutorialScreen(
+    modifier: Modifier = Modifier.fillMaxSize(),
+    gameState: GameState = GameState(),
+    onRestart: () -> Unit,
+    onReturnToMenu: () -> Unit,
+    dismissTutorial: () -> Unit
+
+){
+    Box(modifier = modifier, contentAlignment = Alignment.Center){
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .clip(RoundedCornerShape(8.dp))
+                .background(MaterialTheme.colorScheme.background.copy(alpha = 1f))
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                text = stringResource(Res.string.tutorial_title),
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp
+            )
+
+            Spacer(Modifier.height(8.dp))
+
+            Text(text = stringResource(Res.string.tutorial_message1))
+            Spacer(Modifier.height(8.dp))
+            Text(text = stringResource(Res.string.tutorial_message2))
+            Spacer(Modifier.height(8.dp))
+            Text(text = stringResource(Res.string.tutorial_message3))
+            Spacer(Modifier.height(8.dp))
+            Text(text = stringResource(Res.string.good_luck))
+            Spacer(Modifier.height(8.dp))
+
+
+            Button(onClick = dismissTutorial ) {
+                Text(text = stringResource(Res.string.im_ready))
             }
 
             Button(onClick = onReturnToMenu) {
