fbee5acf22b12cb50db640453c60fbd1e394c110
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-04 19:53:10 -0700

    Update GameScreen.kt

--- composeApp/src/commonMain/kotlin/presentation/components/GameScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/GameScreen.kt
@@ -1,18 +1,29 @@
 package presentation.components
 
-import model.GameState
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
+import model.GameState
 
 @Composable
 fun GameScreen(
@@ -20,7 +31,9 @@ fun GameScreen(
     rows: Int = 4,
     columns: Int = 3,
     gameState: GameState,
-    onCardFlipped: (Int) -> Unit
+    onCardFlipped: (Int) -> Unit,
+    onRestart: () -> Unit,
+    onReturnToMenu: () -> Unit
 ){
     Column(
         modifier = modifier,
@@ -52,6 +65,34 @@ fun GameScreen(
         }
     }
 
+    AnimatedVisibility(
+        visible = gameState.gameWon,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ){
+        GameWonScreen(
+            modifier = modifier,
+            gameState = gameState,
+            onRestart = { onRestart() },
+            onReturnToMenu = { onReturnToMenu() }
+        )
+    }
+
+    AnimatedVisibility(
+        visible = gameState.gameLost,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ){
+        GameLostScreen(
+            modifier = modifier,
+            gameState = gameState,
+            onRestart = { onRestart() },
+            onReturnToMenu = { onReturnToMenu() }
+        )
+    }
+
+
+
 
 
 }
@@ -64,17 +105,25 @@ fun GameLostScreen(
     onReturnToMenu: () -> Unit
 ){
     Column(
-        modifier = modifier,
+        modifier = Modifier
+            .wrapContentSize()
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.background.copy(alpha = 0.75f))
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Game Over")
         Text(text = "Three strikes and you're out...")
         Text(text = "Points: ${gameState.points}")
-        Button(onClick = onRestart) {
+        Button(
+            onClick = onRestart
+        ) {
             Text(text = "Restart")
         }
-        Button(onClick = onReturnToMenu) {
+        Button(
+            onClick = onReturnToMenu
+        ) {
             Text(text = "Return To Menu")
         }
     }
@@ -82,14 +131,16 @@ fun GameLostScreen(
 
 @Composable
 fun GameWonScreen(
-    modifier: Modifier = Modifier.fillMaxSize(),
+    modifier: Modifier = Modifier.wrapContentSize(),
     gameState: GameState = GameState(),
     onRestart: () -> Unit,
     onReturnToMenu: () -> Unit
 
 ){
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.background)
+            .padding(8.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
