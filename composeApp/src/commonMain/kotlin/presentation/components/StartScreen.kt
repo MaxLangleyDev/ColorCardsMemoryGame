9fe4702ed4abbd325c24d058d60e0165d3df1aa5
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-07 16:03:50 -0700

    Update StartScreen.kt

--- composeApp/src/commonMain/kotlin/presentation/components/StartScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/StartScreen.kt
@@ -4,14 +4,18 @@ package presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import colorcardsmemorygame.composeapp.generated.resources.Res
 import colorcardsmemorygame.composeapp.generated.resources.start_game
+import colorcardsmemorygame.composeapp.generated.resources.tutorial_title
 import model.GameState
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.stringResource
@@ -21,7 +25,8 @@ fun StartScreen(
     modifier: Modifier = Modifier.fillMaxSize(),
     gameState: GameState,
     setupGame: (amountOfCards: Int) -> Unit = {},
-    startGame: () -> Unit = {}
+    startGame: () -> Unit = {},
+    showTutorial: () -> Unit = {}
 ) {
     Column(
         modifier = modifier,
@@ -36,6 +41,16 @@ fun StartScreen(
             },
         ){
             Text(text = stringResource(Res.string.start_game))
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = {
+                showTutorial()
+            },
+        ){
+            Text(text = stringResource(Res.string.tutorial_title))
         }
 
     }
