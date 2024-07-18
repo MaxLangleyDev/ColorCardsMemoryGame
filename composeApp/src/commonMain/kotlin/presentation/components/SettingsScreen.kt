d658435ceb964eebb422f9cdd1ba55fe289e38c8
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-07-18 10:37:50 -0700

    Update SettingsScreen.kt

--- composeApp/src/commonMain/kotlin/presentation/components/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/SettingsScreen.kt
@@ -3,11 +3,18 @@ package presentation.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,7 +24,10 @@ import model.GameState
 fun SettingsScreen(
     modifier: Modifier = Modifier,
     gameState: GameState = GameState(),
-    onDismissSettings: () -> Unit
+    onDismissSettings: () -> Unit,
+    setPregameCountdown: (Int) -> Unit,
+    setGameCountdown: (Int) -> Unit,
+    setGameOverOnThreeStrikes: (Boolean) -> Unit
 ){
 
     Column(
@@ -30,6 +40,23 @@ fun SettingsScreen(
         Text(text = "Settings")
 
         Spacer(modifier = Modifier.height(8.dp))
+
+        // Game over on three strikes
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            var radioSelected by remember { mutableStateOf(false) }
+            Text(text = "Three Strikes = Game Over")
+            RadioButton(
+                selected = radioSelected,
+                onClick = {
+                    radioSelected = !radioSelected
+                    setGameOverOnThreeStrikes(radioSelected)
+                }
+            )
+        }
 
 
 
